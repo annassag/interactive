7e14b028eaa35910d8f3155278628157fd7024c4
Anna Sagstrom <annasagstrom@Moniques-MBP-2.wireless.yale.internal>
2019-03-04 13:32:48 -0500

    ny

---
+++ exercise4/js/script.js
@@ -0,0 +1,32 @@
+
+
+$(document).ready(function(){
+
+
+
+ 
+
+console.log(stop);
+
+
+$(".background").click(function () {
+   $(this).toggleClass(".background2");
+});
+
+
+
+
+
+
+   // Part 3
+   // To come! :)
+
+setInterval( function() {
+  toggleClass();
+}, 5000);
+
+
+
+
+
+});
